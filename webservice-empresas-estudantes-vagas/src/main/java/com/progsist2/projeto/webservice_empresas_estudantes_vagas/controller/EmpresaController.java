46d9bb03337123d434604248befc5697e95e1b56
Gabriel Freitas <gfcmiwa@gmail.com>
2025-11-17 17:20:55 +0000

    att geral

--- webservice-empresas-estudantes-vagas/src/main/java/com/progsist2/projeto/webservice_empresas_estudantes_vagas/controller/EmpresaController.java
+++ webservice-empresas-estudantes-vagas/src/main/java/com/progsist2/projeto/webservice_empresas_estudantes_vagas/controller/EmpresaController.java
@@ -5,7 +5,7 @@ import com.progsist2.projeto.webservice_empresas_estudantes_vagas.repository.Emp
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import java.util.List;    
 
 @RestController
 @RequestMapping("/empresas")
