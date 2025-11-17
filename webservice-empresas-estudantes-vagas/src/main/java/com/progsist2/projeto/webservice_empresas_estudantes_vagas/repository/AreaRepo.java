346a13fdf9be8d31c431e499c7344a818fb14a10
Gabriel Freitas <gfcmiwa@gmail.com>
2025-11-17 17:56:53 +0000

    atualização geral

--- webservice-empresas-estudantes-vagas/src/main/java/com/progsist2/projeto/webservice_empresas_estudantes_vagas/repository/AreaRepo.java
+++
@@ -1,6 +0,0 @@
-package com.progsist2.projeto.webservice_empresas_estudantes_vagas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.progsist2.projeto.webservice_empresas_estudantes_vagas.model.Area;
-
-public interface AreaRepo extends JpaRepository<Area, Long> {}
